0e7b20bafee08d1788e4e204bfc58b2a48accbc8
Aniket Sharma <aniket.sharma@moreideas.ae>
2023-12-11 18:07:04 +0400

    next js

---
+++ src/app/about/page.js
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+export default function About() {
+   return (
+      <main className="row">
+         <div className="text-center  pt-4">
+            <h1 className="text-2xl font-bold">Aniket Sharma</h1>
+         </div>
+         <div className="w-500 mx-auto my-4">
+            <ul className="flex justify-evenly ">
+               <li className="underline underline-offset-8"><Link href="/">Home</Link></li>
+               <li className="underline underline-offset-8"><Link href="/about">About</Link></li>
+               <li className="underline underline-offset-8"><Link href="/work">Work</Link></li>
+               <li className="underline underline-offset-8"><Link href="/contact">Contact</Link></li>
+            </ul>
+         </div>
+         <div className="flex flex-col items-center justify-between p-24">
+            <h3 className="font-bold tracking-widest">Hi, I'm Aniket Sharma from India.</h3>
+         </div>
+      </main>
+   );
+}
